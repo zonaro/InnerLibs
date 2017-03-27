0f14d6bb42f9c8381471becbb5b6f237dba62da5
Gabriel Zonaro <zonaro@outlook.com>
2017-03-27 17:36:42 -0300

    NotificationForm alteracoes

--- innerlibs1/NotificationForm.designer.vb
+++ innerlibs1/NotificationForm.designer.vb
@@ -28,6 +28,7 @@ Partial Class NotificationForm
         Me.OKButton = New System.Windows.Forms.Button()
         Me.CloseButton = New System.Windows.Forms.Button()
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.SuspendLayout()
         '
         'MessageLabel
@@ -38,7 +39,7 @@ Partial Class NotificationForm
         Me.MessageLabel.BackColor = System.Drawing.Color.Transparent
         Me.MessageLabel.Location = New System.Drawing.Point(4, 27)
         Me.MessageLabel.Name = "MessageLabel"
-        Me.MessageLabel.Size = New System.Drawing.Size(184, 77)
+        Me.MessageLabel.Size = New System.Drawing.Size(219, 123)
         Me.MessageLabel.TabIndex = 0
         Me.MessageLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
@@ -54,10 +55,10 @@ Partial Class NotificationForm
         Me.OKButton.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
         Me.OKButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.OKButton.ForeColor = System.Drawing.Color.Black
-        Me.OKButton.Location = New System.Drawing.Point(101, 106)
+        Me.OKButton.Location = New System.Drawing.Point(143, 152)
         Me.OKButton.Margin = New System.Windows.Forms.Padding(2)
         Me.OKButton.Name = "OKButton"
-        Me.OKButton.Size = New System.Drawing.Size(85, 29)
+        Me.OKButton.Size = New System.Drawing.Size(78, 29)
         Me.OKButton.TabIndex = 1
         Me.OKButton.Text = "OK"
         Me.OKButton.UseVisualStyleBackColor = False
@@ -71,7 +72,7 @@ Partial Class NotificationForm
         Me.CloseButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.CloseButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.CloseButton.ForeColor = System.Drawing.Color.Transparent
-        Me.CloseButton.Location = New System.Drawing.Point(167, 3)
+        Me.CloseButton.Location = New System.Drawing.Point(202, 3)
         Me.CloseButton.Name = "CloseButton"
         Me.CloseButton.Size = New System.Drawing.Size(21, 20)
         Me.CloseButton.TabIndex = 2
@@ -89,12 +90,22 @@ Partial Class NotificationForm
         Me.Label1.Text = "0"
         Me.Label1.Visible = False
         '
+        'TextBox1
+        '
+        Me.TextBox1.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox1.Location = New System.Drawing.Point(7, 157)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(131, 20)
+        Me.TextBox1.TabIndex = 4
+        '
         'NotificationForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.White
-        Me.ClientSize = New System.Drawing.Size(191, 141)
+        Me.ClientSize = New System.Drawing.Size(226, 187)
+        Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.CloseButton)
         Me.Controls.Add(Me.OKButton)
@@ -113,4 +124,5 @@ Partial Class NotificationForm
     Private WithEvents OKButton As Windows.Forms.Button
     Private WithEvents CloseButton As Windows.Forms.Button
     Friend WithEvents Label1 As Windows.Forms.Label
+    Friend WithEvents TextBox1 As Windows.Forms.TextBox
 End Class
