13e35a92d4571a89cd64bd65d1be771e923ef2f9
Gabriel Zonaro <zonaro@outlook.com>
2021-09-24 15:32:06 -0300

    Update Printer.vb

--- innerlibs1/Printer/Printer.vb
+++ innerlibs1/Printer/Printer.vb
@@ -645,7 +645,7 @@ Namespace Printer
         ''' <param name="values"></param>
         ''' <returns></returns>
         Public Shadows Function WriteLine(ParamArray values As String()) As Printer
-            values = If(values, {}).Where(Function(x) x.IsNotBlank())
+            values = If(values, {}).Where(Function(x) x.IsNotBlank()).ToArray()
             If values.Any() Then
                 WriteLine(values.Join(vbNewLine))
             End If
