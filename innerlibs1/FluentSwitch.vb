d4a0ca010eacf9d6bf40044d3534217e11b30977
Gabriel Zonaro <zonaro@outlook.com>
2021-09-28 13:50:12 -0300

    Update FluentSwitch.vb

--- innerlibs1/FluentSwitch.vb
+++ innerlibs1/FluentSwitch.vb
@@ -4,51 +4,51 @@ Imports System.Runtime.CompilerServices
 Public Module FluentSwitchExt
 
     <Extension()>
-    Public Function Switch(Of OutT, InT)(Input As InT) As FluentSwitch(Of InT, OutT)
-        Return New FluentSwitch(Of InT, OutT)(Input)
+    Public Function Switch(Of TestType, ReturnType)(Input As TestType) As FluentSwitch(Of TestType, ReturnType)
+        Return New FluentSwitch(Of TestType, ReturnType)(Input)
     End Function
 
     <Extension()>
-    Public Function Switch(Of OutT, InT)(Input As InT, Test As Action(Of FluentSwitch(Of InT, OutT))) As OutT
-        Dim a = Input.Switch(Of OutT)()
+    Public Function Switch(Of TestType, ReturnType)(Input As TestType, Test As Action(Of FluentSwitch(Of TestType, ReturnType))) As ReturnType
+        Dim a = Input.Switch(Of ReturnType)()
         Test(a)
         Return a.GetValue()
     End Function
 End Module
 
-Public Class FluentSwitch(Of T1, T2)
+Public Class FluentSwitch(Of TestType, ReturnType)
 
-    Private dic As New Dictionary(Of T1, T2)
+    Private dic As New Dictionary(Of TestType, ReturnType)
 
-    Private input As T1 = Nothing
-    Private defaultv As T2 = Nothing
+    Private input As TestType = Nothing
+    Private defaultv As ReturnType = Nothing
 
-    Public Sub New(Input As T1, Optional DefaultValue As T2 = Nothing)
+    Public Sub New(Input As TestType, Optional DefaultValue As ReturnType = Nothing)
         Me.input = Input
         Me.defaultv = DefaultValue
     End Sub
 
-    Public Function [Case](Value As T1, ReturnValue As T2) As FluentSwitch(Of T1, T2)
+    Public Function [Case](Value As TestType, ReturnValue As ReturnType) As FluentSwitch(Of TestType, ReturnType)
         Return [Case]({Value}, ReturnValue)
     End Function
-    Public Function [Case](Values As IEnumerable(Of T1), ReturnValue As T2) As FluentSwitch(Of T1, T2)
-        For Each item As T1 In Values
+    Public Function [Case](Values As IEnumerable(Of TestType), ReturnValue As ReturnType) As FluentSwitch(Of TestType, ReturnType)
+        For Each item As TestType In Values
             dic.Set(item, ReturnValue)
         Next
         Return Me
     End Function
 
 
-    Public Function [Default](ReturnValue As T2) As FluentSwitch(Of T1, T2)
+    Public Function [Default](ReturnValue As ReturnType) As FluentSwitch(Of TestType, ReturnType)
         defaultv = ReturnValue
         Return Me
     End Function
 
-    Public Function GetValue() As T2
+    Public Function GetValue() As ReturnType
         Return dic.GetValueOr(input, defaultv)
     End Function
 
-    Public Shared Widening Operator CType(FS As FluentSwitch(Of T1, T2)) As T2
+    Public Shared Widening Operator CType(FS As FluentSwitch(Of TestType, ReturnType)) As ReturnType
         If FS Is Nothing Then Return Nothing
         Return FS.GetValue()
     End Operator
