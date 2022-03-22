8be4ed36c61c181342246da6ef81553781f8727f
Gabriel Zonaro <zonaro@outlook.com>
2022-03-22 16:31:35 -0300

    -

--- InnerLibs/DebugTextWriter.cs
+++ InnerLibs/DebugTextWriter.cs
@@ -19,78 +19,30 @@ namespace InnerLibs
                 Debug.Write(Encoding.Unicode.GetString(buffer, offset, count));
             }
 
-            public override bool CanRead
-            {
-                get
-                {
-                    return false;
-                }
-            }
+            public override bool CanRead => false;
 
-            public override bool CanSeek
-            {
-                get
-                {
-                    return false;
-                }
-            }
+            public override bool CanSeek => false;
 
-            public override bool CanWrite
-            {
-                get
-                {
-                    return true;
-                }
-            }
+            public override bool CanWrite => true;
 
-            public override long Length
-            {
-                get
-                {
-                    throw bad_op;
-                }
-            }
+            public override long Length => throw bad_op;
 
-            public override int Read(byte[] buffer, int offset, int count)
-            {
-                throw bad_op;
-            }
+            public override int Read(byte[] buffer, int offset, int count) => throw bad_op;
 
-            public override long Seek(long offset, SeekOrigin origin)
-            {
-                throw bad_op;
-            }
+            public override long Seek(long offset, SeekOrigin origin) => throw bad_op;
 
-            public override void SetLength(long value)
-            {
-                throw bad_op;
-            }
+            public override void SetLength(long value) => throw bad_op;
 
-            public override void Flush()
-            {
-                Debug.Flush();
-            }
+            public override void Flush() => Debug.Flush();
 
             public override long Position
             {
-                get
-                {
-                    throw bad_op;
-                }
+                get => throw bad_op;
 
-                set
-                {
-                    throw bad_op;
-                }
+                set => throw bad_op;
             }
 
-            private static InvalidOperationException bad_op
-            {
-                get
-                {
-                    return new InvalidOperationException();
-                }
-            }
+            private static InvalidOperationException bad_op => new InvalidOperationException("Operation not supported");
         }
     }
 }
