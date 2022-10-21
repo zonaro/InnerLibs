a8c20cb84616735d79e9900e07352c9566c97dd0
Zonaro <zonaro@outlook.com>
2022-10-21 17:19:28 -0300

    HtmlTag.CreateTable

--- InnerLibs/HtmlTag.cs
+++ InnerLibs/HtmlTag.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 
 namespace InnerLibs
 {
@@ -10,8 +11,8 @@ namespace InnerLibs
     /// </summary>
     public class HtmlTag
     {
-        private Dictionary<string, string> attrs = new Dictionary<string, string>();
         private string _innerHtml;
+        private Dictionary<string, string> attrs = new Dictionary<string, string>();
 
         public HtmlTag()
         {
@@ -57,26 +58,6 @@ namespace InnerLibs
             set => Class = (value ?? Array.Empty<string>()).SelectJoinString(" ");
         }
 
-        public HtmlTag AddClass(string ClassName)
-        {
-            if (ClassName.IsNotBlank() && ClassName.IsNotIn(ClassList, StringComparer.InvariantCultureIgnoreCase))
-            {
-                Class = Class.Append(" " + ClassName);
-            }
-
-            return this;
-        }
-        public HtmlTag RemoveClass(string ClassName)
-        {
-            if (ClassName.IsNotBlank() && ClassName.IsIn(ClassList, StringComparer.InvariantCultureIgnoreCase))
-            {
-                ClassList = ClassList.Where(x => x.ToLower() != ClassName.ToLower()).ToArray();
-            }
-
-            return this;
-        }
-
-
         public string InnerHtml
         {
             get => _innerHtml;
@@ -103,32 +84,108 @@ namespace InnerLibs
         public static HtmlTag CreateAnchor(string URL, string Text, string Target = "_self", object htmlAttributes = null) => new HtmlTag("a", htmlAttributes, Text).SetAttr("href", URL, true).SetAttr("target", Target, true);
 
         public static HtmlTag CreateImage(string URL, object htmlAttributes = null) => new HtmlTag("img", htmlAttributes, null).SetAttr("src", URL, true).With(x =>
-          {
-              x.SelfCloseTag = true;
-          });
+            {
+                x.SelfCloseTag = true;
+            });
 
         public static HtmlTag CreateInput(string Name, string Value = null, string Type = "text", object htmlAttributes = null) => new HtmlTag("input", htmlAttributes, null).With(x =>
-          {
-              x.SelfCloseTag = true;
-              x.SetAttr("name", Name, true)
-               .SetAttr("value", Value, true)
-               .SetAttr("type", Type.IfBlank("text"), true);
-          });
+            {
+                x.SelfCloseTag = true;
+                x.SetAttr("name", Name, true)
+                .SetAttr("value", Value, true)
+                .SetAttr("type", Type.IfBlank("text"), true);
+            });
 
         public static HtmlTag CreateOption(string Name, string Value = null, bool Selected = false) => new HtmlTag("option", null, null).With(x =>
-          {
-              x.InnerHtml = Name.RemoveHTML();
-              x.SetAttr("value", Value)
-               .SetProp("selected", Selected);
-          });
+            {
+                x.InnerHtml = Name.RemoveHTML();
+                x.SetAttr("value", Value)
+                .SetProp("selected", Selected);
+            });
+
+        public static HtmlTag CreateTable<TPoco>(List<TPoco> rows, bool header, string IDProperty, params string[] Properties) where TPoco : class
+        {
+            TPoco h = null;
+            if (header)
+            {
+                try
+                {
+                    var nomes = typeof(TPoco).GetProperties().Select(x => x.Name);
+                    h = Activator.CreateInstance<TPoco>();
+                    foreach (var item in nomes)
+                    {
+                        h.SetPropertyValue(item, item);
+                    }
+                }
+                catch
+                {
+                }
+
+            }
+            return CreateTable(rows, h, IDProperty, Properties);
+        }
+
+
+        //public static HtmlTag CreateTable<TPoco>(List<TPoco> items, TPoco header, Expression<Func<TPoco,string>> IDExpression, params Expression<Func<TPoco, string>>[] properties ) where TPoco : class
+        public static HtmlTag CreateTable<TPoco>(List<TPoco> rows) where TPoco : class => CreateTable(rows, false, null, null);
+        public static HtmlTag CreateTable<TPoco>(List<TPoco> rows, TPoco header, string IDProperty, params string[] properties) where TPoco : class
+        {
+            HtmlTag tag = new HtmlTag("table");
+
+            if (properties?.Any() == true)
+            {
+                properties = properties.Where(x => x.IsNotBlank()).ToArray();
+            }
+            else
+            {
+                properties = typeof(TPoco).GetProperties().Select(x => x.Name).ToArray();
+            }
+
+            Func<TPoco, IEnumerable<PropertyInfo>> props = (TPoco t) => t.GetProperties().Where(x => x.Name.IsAny(StringComparison.InvariantCultureIgnoreCase, properties));
+
+            if (header != null)
+            {
+                tag.InnerHtml += props(header).SelectJoinString(x => (x.GetValue(header)?.ToString() ?? x.Name).WrapInTag("th")).WrapInTag("tr").ToString().WrapInTag("thead");
+            }
+
+            if (rows != null && rows.Any())
+            {
+                tag.InnerHtml += rows.SelectJoinString(row => props(row).SelectJoinString(column => column.GetValue(row)?.ToString().WrapInTag("td")).WrapInTag("tr").With(w =>
+                {
+                    if (IDProperty.IsNotBlank()) w.SetAttr("ID", row.GetPropertyValue<object, TPoco>(IDProperty).ToString());
+                }).ToString()).WrapInTag("tbody");
+            }
+
+            return tag;
+        }
 
         public static implicit operator string(HtmlTag Tag) => Tag?.ToString();
+
+        public HtmlTag AddClass(string ClassName)
+        {
+            if (ClassName.IsNotBlank() && ClassName.IsNotIn(ClassList, StringComparer.InvariantCultureIgnoreCase))
+            {
+                Class = Class.Append(" " + ClassName);
+            }
+
+            return this;
+        }
 
         public bool HasAttribute(string AttrName) => Attributes.ContainsKey(AttrName);
 
         public HtmlTag RemoveAttr(string AttrName)
         {
             Attributes.SetOrRemove(AttrName, null, true);
+            return this;
+        }
+
+        public HtmlTag RemoveClass(string ClassName)
+        {
+            if (ClassName.IsNotBlank() && ClassName.IsIn(ClassList, StringComparer.InvariantCultureIgnoreCase))
+            {
+                ClassList = ClassList.Where(x => x.ToLower() != ClassName.ToLower()).ToArray();
+            }
+
             return this;
         }
 
@@ -143,7 +200,7 @@ namespace InnerLibs
         public override string ToString()
         {
             TagName = TagName.RemoveAny("/", @"\").IfBlank("div");
-            return $"<{TagName} {Attributes.SelectJoinString(x => x.Key == x.Value ? x.Key.ToLower() : $"{x.Key.ToLower()}={x.Value.Wrap()}")} " + (SelfCloseTag ? "/>" : $">{InnerHtml}</{TagName}>");
+            return $"<{TagName}{Attributes.SelectJoinString(x => x.Key == x.Value ? x.Key.ToLower() : $"{x.Key.ToLower()}={x.Value.Wrap()}", " ").PrependIf(" ", b => b.IsNotBlank())}" + (SelfCloseTag ? " />" : $">{InnerHtml}</{TagName}>");
         }
     }
 }
